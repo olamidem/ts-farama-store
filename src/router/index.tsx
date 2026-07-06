2e7ccf002a29273adb862b1566ca00d9d771f15f
olamind <olamxii@gmail.com>
2026-07-06 16:08:41 +0300

    refactor(router): introduce nested layout routing

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -7,6 +7,7 @@ import {
 import LoginScreen from "../pages/LoginScreen";
 import { requireAuth } from "./guard";
 import Dashboard from "../pages/Dashboard/Dashboard";
+import DashboardLayout from "../layout/DashboardLayout";
 
 const rootRoute = createRootRoute();
 const loginRoute = createRoute({
@@ -15,19 +16,41 @@ const loginRoute = createRoute({
   component: LoginScreen,
 });
 
-const dashboardRoute = createRoute({
+/**
+ * Protected Application Layout
+ */
+const appLayoutRoute = createRoute({
   getParentRoute: () => rootRoute,
-  path: "/dashboard",
+  id: "app",
   beforeLoad: requireAuth,
+  component: DashboardLayout,
+});
+
+const dashboardRoute = createRoute({
+  getParentRoute: () => appLayoutRoute,
+  path: "/dashboard",
   component: Dashboard,
 });
 
-const routeTree = rootRoute.addChildren([loginRoute, dashboardRoute]);
+/**
+ * Route Tree
+ */
+const routeTree = rootRoute.addChildren([
+  loginRoute,
+  appLayoutRoute.addChildren([dashboardRoute]),
+]);
+
+/**
+ * Router
+ */
 export const router = createRouter({
   routeTree,
   history: createBrowserHistory(),
 });
 
+/**
+ * Router Types
+ */
 declare module "@tanstack/react-router" {
   interface Register {
     router: typeof router;
