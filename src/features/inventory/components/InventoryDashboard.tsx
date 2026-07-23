d73ec87efc34bf263fbf1dc73e188cdf885fd4db
olamind <olamxii@gmail.com>
2026-07-23 15:10:26 +0300

    feat(inventory): build inventory dashboard interface

--- src/features/inventory/components/InventoryDashboard.tsx
+++ src/features/inventory/components/InventoryDashboard.tsx
@@ -36,7 +36,11 @@ import { StockTransferFormPanel } from "./StockTransferFormPanel";
 import type { InventorySettings } from "../types/inventory";
 import StockCountPanel from "./StockCountPanel";
 import ProductLedgerPanel from "./ProductLedgerPanel";
-import { InventoryReportsPanel, InventorySettingsFormPanel, LowStockAlertsPanel } from "./InventoryReportsAndAlerts";
+import {
+  InventoryReportsPanel,
+  InventorySettingsFormPanel,
+  LowStockAlertsPanel,
+} from "./InventoryReportsAndAlerts";
 import { useCreateStockAdjustment } from "../hooks/useInventoryMutations";
 
 export const InventoryDashboard = () => {
